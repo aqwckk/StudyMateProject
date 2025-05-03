ba34a3c1070c3c09b9e60dbac890fa30c69ea9c1
Кирилл Гладких <gladkih.kirill.20588@gmail.com>
2025-05-03 14:15:29 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyMateProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
