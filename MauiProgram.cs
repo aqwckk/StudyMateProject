93b6843978a7d30b2a627511bb321994f8689946
nIkvo <ogloblinnv@gmail.com>
2025-06-04 22:55:18 +0500

    secret

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -16,7 +16,7 @@ namespace StudyMateProject
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
